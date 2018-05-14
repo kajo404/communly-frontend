e871c43bc121ae703b1f866cc129cb1edf3a5b8c
Yasna Mindilikova <yasna.mindilikova@gmail.com>
2018-05-14 15:54:06 +0300

    feat: Some starter things

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: '#597ec0',
+    primary1Color: '#9ab1d9',
+    primary2Color: '#9ab1d9'
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="c-main-wrapper">
+          <h1> Communly test: color scheme </h1>
+          <img className="c-logo" src={logo} />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
