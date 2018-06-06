2f2840d25073356e5befcb89978259b027bdf5ee
Lara Marie Reimer <laramarie@reimer.io>
2018-06-05 17:12:34 -0700

    Refactor scss and use less if statements

--- src/pages/announcements-page/announcements-page.jsx
+++ src/pages/announcements-page/announcements-page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './announcements.scss';
 import AnnouncementComponent from './announcement';
 import AnnouncementsService from '../../services/announcements-service';
 import NewAnnouncementModal from './new-announcement-modal';
@@ -15,7 +16,8 @@ const styles = {
 const dividerStyles = {
   backgroundColor: '#314f81',
   height: 2,
-  width: '98%'
+  width: '100%',
+  margin: 30
 };
 
 class Announcements extends React.Component {
@@ -34,6 +36,31 @@ class Announcements extends React.Component {
     this.savedNewAnnouncement = this.savedNewAnnouncement.bind(this);
   }
 
+  // Getters
+
+  get shouldShowAllAnnouncements() {
+    return (
+      this.state.selectedValue === 0 &&
+      this.state.adminAnnouncements.length > 0 &&
+      this.state.normalAnnouncements.length > 0
+    );
+  }
+
+  get shouldShowAdminAnnouncements() {
+    return (
+      (this.state.selectedValue === 0 || this.state.selectedValue === 1) &&
+      this.state.adminAnnouncements.length > 0
+    );
+  }
+
+  get shouldShowResidentAnnouncements() {
+    return (
+      (this.state.selectedValue === 0 || this.state.selectedValue === 2) &&
+      this.state.normalAnnouncements.length > 0
+    );
+  }
+
+  // Functionality
   updateAnnouncements() {
     AnnouncementsService.getAnnouncements()
       .then(data => this.filterAnnouncements(data.announcements))
@@ -45,17 +72,16 @@ class Announcements extends React.Component {
   }
 
   filterAnnouncements(announcements) {
-    const adminAnnouncementsFiltered = announcements.filter(function(
-      announcement
-    ) {
+    console.log(announcements);
+    const adminAnnouncementsFiltered = announcements.filter(announcement => {
       return announcement.author.roles.includes('admin');
     });
 
-    const normalAnnouncementsFiltered = announcements.filter(function(
-      announcement
-    ) {
+    console.log(adminAnnouncementsFiltered);
+    const normalAnnouncementsFiltered = announcements.filter(announcement => {
       return !announcement.author.roles.includes('admin');
     });
+    console.log(normalAnnouncementsFiltered);
 
     this.setState({
       adminAnnouncements: adminAnnouncementsFiltered,
@@ -68,126 +94,59 @@ class Announcements extends React.Component {
   };
 
   render() {
-    if (
-      this.state.adminAnnouncements.length > 0 &&
-      this.state.normalAnnouncements.length > 0 &&
-      this.state.selectedValue === 0
-    ) {
-      return (
-        <div>
-          <div className="p-announcements-dropdown">
-            <DropDownMenu
-              value={this.state.selectedValue}
-              onChange={this.handleChange}
-              style={styles}
-            >
-              <MenuItem value={0} primaryText="All Posts" />
-              <MenuItem value={1} primaryText="Only Admin Posts" />
-              <MenuItem value={2} primaryText="Only Resident Posts" />
-            </DropDownMenu>
-          </div>
-          <div className="p-announcements-container">
-            <h1>Admin Posts</h1>
-            <br />
-            <div className="p-announcements-content">
-              {this.state.adminAnnouncements.map((announcement, index) => (
-                <AnnouncementComponent
-                  announcement={announcement}
-                  key={index}
-                />
-              ))}
-            </div>
-          </div>
-          <Divider style={dividerStyles} />
-          <div className="p-announcements-lower-container">
-            <h1>Resident Posts</h1>
-            <br />
-            <div className="p-announcements-content">
-              {this.state.normalAnnouncements.map((announcement, index) => (
-                <AnnouncementComponent
-                  announcement={announcement}
-                  key={index}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="p-announcements-add-button">
-            <NewAnnouncementModal
-              savedNewAnnouncement={this.savedNewAnnouncement}
-            />
-          </div>
+    return (
+      <div>
+        <div className="p-announcements-dropdown">
+          <DropDownMenu
+            value={this.state.selectedValue}
+            onChange={this.handleChange}
+            style={styles}
+          >
+            <MenuItem value={0} primaryText="All Posts" />
+            <MenuItem value={1} primaryText="Only Admin Posts" />
+            <MenuItem value={2} primaryText="Only Resident Posts" />
+          </DropDownMenu>
         </div>
-      );
-    } else if (
-      this.state.adminAnnouncements.length > 1 &&
-      this.state.selectedValue === 1
-    ) {
-      return (
-        <div>
-          <div className="p-announcements-dropdown">
-            <DropDownMenu
-              value={this.state.selectedValue}
-              onChange={this.handleChange}
-              style={styles}
-            >
-              <MenuItem value={0} primaryText="All Posts" />
-              <MenuItem value={1} primaryText="Only Admin Posts" />
-              <MenuItem value={2} primaryText="Only Resident Posts" />
-            </DropDownMenu>
-          </div>
-          <div className="p-announcements-container">
-            <h1>Admin Posts</h1>
-            <br />
-            <div className="p-announcements-content">
-              {this.state.adminAnnouncements.map((announcement, index) => (
-                <AnnouncementComponent
-                  announcement={announcement}
-                  key={index}
-                />
-              ))}
+        <div className="p-announcements-container">
+          {this.shouldShowAdminAnnouncements ? (
+            <div>
+              <h1>Admin Posts</h1>
+              <br />
+              <div className="p-announcements-content">
+                {this.state.adminAnnouncements.map((announcement, index) => (
+                  <AnnouncementComponent
+                    announcement={announcement}
+                    key={index}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="p-announcements-add-button">
-            <NewAnnouncementModal
-              savedNewAnnouncement={this.savedNewAnnouncement}
-            />
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="p-announcements-dropdown">
-            <DropDownMenu
-              value={this.state.selectedValue}
-              onChange={this.handleChange}
-              style={styles}
-            >
-              <MenuItem value={0} primaryText="All Posts" />
-              <MenuItem value={1} primaryText="Only Admin Posts" />
-              <MenuItem value={2} primaryText="Only Resident Posts" />
-            </DropDownMenu>
-          </div>
-          <div className="p-announcements-container">
-            <h1>Resident Posts</h1>
-            <br />
-            <div className="p-announcements-content">
-              {this.state.normalAnnouncements.map((announcement, index) => (
-                <AnnouncementComponent
-                  announcement={announcement}
-                  key={index}
-                />
-              ))}
+          ) : null}
+          {this.shouldShowAllAnnouncements ? (
+            <Divider style={dividerStyles} />
+          ) : null}
+          {this.shouldShowResidentAnnouncements ? (
+            <div>
+              <h1>Resident Posts</h1>
+              <br />
+              <div className="p-announcements-content">
+                {this.state.normalAnnouncements.map((announcement, index) => (
+                  <AnnouncementComponent
+                    announcement={announcement}
+                    key={index}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="p-announcements-add-button">
-            <NewAnnouncementModal
-              savedNewAnnouncement={this.savedNewAnnouncement}
-            />
-          </div>
+          ) : null}
         </div>
-      );
-    }
+        <div className="p-announcements-add-button">
+          <NewAnnouncementModal
+            savedNewAnnouncement={this.savedNewAnnouncement}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
