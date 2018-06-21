ab956a60e4166591972fa828cc721a055cfaa0e4
Yasna Mindilikova <yasna.mindilikova@sinnerschrader.com>
2018-06-21 22:39:56 +0200

    Twinkle star animation

--- src/pages/user-registration.js
+++ src/pages/user-registration.js
@@ -53,6 +53,30 @@ class UserRegistration extends React.Component {
     };
   }
 
+  componentDidMount() {
+    const contentEl = document.querySelector('.p-registration__content');
+    const starsContainerEl = document.createElement('div');
+    starsContainerEl.classList.add('p-registration__stars');
+    const starsCount = contentEl.clientWidth * contentEl.clientHeight / 20000;
+    for (let i = 0; i < starsCount; i++) {
+      const star = document.createElement('div');
+      star.classList.add('p-registration__star');
+      const pos = this.randomPosInside(contentEl);
+      star.style.left = pos.x + 'px';
+      star.style.top = pos.y + 'px';
+      star.style.animationDuration = '1.5s';
+      star.style.animationDelay = '-' + Math.random() * 3 + 's';
+      starsContainerEl.appendChild(star);
+    }
+    contentEl.appendChild(starsContainerEl);
+  }
+
+  randomPosInside(el) {
+    const x = Math.floor(Math.random() * el.clientWidth);
+    const y = Math.floor(Math.random() * el.clientHeight);
+    return { x, y };
+  }
+
   onSubmit = event => {
     event.preventDefault();
 
@@ -244,16 +268,34 @@ class UserRegistration extends React.Component {
         </div>
         <div className="p-registration__services">
           <div className="p-registration__icons">
+            <h2 className="p-registration__services-title"> Our services </h2>
             <div className="p-registration__icon-wrapper">
-              <span className="c-icon__description"> Announcements</span>
               <AnnouncementIcon
                 color="#314f81"
-                className="p-registration__icon"
+                className="p-registration__icon margin-right"
               />
+              <p className="c-icon__description">
+                {' '}
+                Announcements <br /> invidunt ut labore et dolore magna aliquyam
+                erat, sed diam voluptua. At vero eos et accusam et justo duo
+                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+                sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+                amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                invidunt ut labore et dolore magna aliquyam erat
+              </p>
             </div>
             <div className="p-registration__icon-wrapper">
-              <span className="c-icon__description"> Task Lists </span>
-              <div>
+              <p className="c-icon__description">
+                {' '}
+                Task Lists <br /> Lorem ipsum dolor sited diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed
+                diam voluptua. At vero eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                invidunt ut labore et dolore magna aliquyam erat
+              </p>
+              <div className="margin-left">
                 <ListAuthorIcon
                   color="#314f81"
                   className="p-registration__icon"
@@ -265,9 +307,8 @@ class UserRegistration extends React.Component {
               </div>
             </div>
             <div className="p-registration__icon-wrapper wider-icons">
-              <span className="c-icon__description"> Activity tracking </span>
-              <div>
-                <TaskAllIcon color="#f5f5f5" className="p-registration__icon" />
+              <div className="margin-right">
+                {/* <TaskAllIcon color="#f5f5f5" className="p-registration__icon" /> */}
                 <TaskDoneIcon
                   color="#314f81"
                   className="p-registration__icon"
@@ -277,7 +318,23 @@ class UserRegistration extends React.Component {
                   className="p-registration__icon"
                 />
               </div>
+              <span className="c-icon__description">
+                {' '}
+                Activity tracking <br /> Lorem ipsum dolor sit amet, consetirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed
+                diam voluptua. At vero eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                invidunt ut labore et dolore magna aliquyam erat{' '}
+              </span>
             </div>
+          </div>
+        </div>
+
+        <div className="p-registration__team">
+          <div className="p-registration__team-wrapper">
+            <h2 className="c-team-headline"> Our Team </h2>
           </div>
         </div>
       </div>
